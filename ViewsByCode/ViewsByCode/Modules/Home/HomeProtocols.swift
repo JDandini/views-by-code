6605d7cea7619ef6478051df8be5ccd72affe143
Javier Castañeda <jdandini.jc@gmail.com>
2021-01-27 08:53:42 -0600

    Fix the swiftlint warnings

--- ViewsByCode/ViewsByCode/Modules/Home/HomeProtocols.swift
+++ ViewsByCode/ViewsByCode/Modules/Home/HomeProtocols.swift
@@ -5,4 +5,29 @@
 //  Created by F J Castaneda Ramos on 27/01/21.
 //
 
-import Foundation
+import UIKit
+
+protocol HomeViewProtocol: class {
+    var presenter: HomePresenterProtocol? { get set }
+}
+
+protocol HomePresenterProtocol: class {
+    var view: HomeViewProtocol? { get set }
+    var interactor: HomeInteractorProtocol? { get set }
+    var router: HomeRouterProtocol? { get set }
+}
+
+protocol HomeInteractorOutput: class {}
+
+protocol HomeInteractorProtocol: class {
+    var presenter: HomeInteractorOutput? { get set }
+    var localDataManager: HomeLocalDataManagerProtocol? { get set }
+}
+
+protocol HomeLocalDataManagerProtocol {}
+
+protocol HomeRouterProtocol {}
+
+protocol HomeBuilderProtocol {
+    static func buildModule() -> UIViewController?
+}
