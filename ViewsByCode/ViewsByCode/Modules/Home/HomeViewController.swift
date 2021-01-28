d22949c9c72d0971f7c3da4321100eed40097b69
Javier Castañeda <jdandini.jc@gmail.com>
2021-01-28 10:01:04 -0600

    Add the home collection view and sort the home module folder

--- ViewsByCode/ViewsByCode/Modules/Home/HomeViewController.swift
+++ ViewsByCode/ViewsByCode/Modules/Home/HomeViewController.swift
@@ -7,11 +7,33 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
     var presenter: HomePresenterProtocol?
+
+    var productCollection: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .lightGray
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        return collection
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(productCollection)
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        productCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        productCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        productCollection.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        productCollection.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 }
