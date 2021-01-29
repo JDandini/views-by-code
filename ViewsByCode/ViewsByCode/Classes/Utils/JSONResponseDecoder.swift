a5c5a50f0c178b87140d6aee282d59f74c85b894
Javier Castañeda <jdandini.jc@gmail.com>
2021-01-29 08:28:43 -0600

    Isolate the JSON decode to a generic struct

--- ViewsByCode/ViewsByCode/Classes/Utils/JSONResponseDecoder.swift
+++ ViewsByCode/ViewsByCode/Classes/Utils/JSONResponseDecoder.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct JSONResponseDecoder<Type: Decodable> {
+    private let decoder = JSONDecoder()
+    func decodeObject(with data: Data)throws -> Type {
+        let objectDecoded = try decoder.decode(Type.self, from: data)
+        return objectDecoded
+    }
+}
