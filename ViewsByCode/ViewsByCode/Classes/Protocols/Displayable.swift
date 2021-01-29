eac2589a34c57f342ef2d07eeed502c106ba4dde
Javier Castañeda <jdandini.jc@gmail.com>
2021-01-29 08:27:58 -0600

    Add the displayable protocol for the make the reuse identifier dynamic

--- ViewsByCode/ViewsByCode/Classes/Protocols/Displayable.swift
+++ ViewsByCode/ViewsByCode/Classes/Protocols/Displayable.swift
@@ -5,4 +5,19 @@
 //  Created by F J Castaneda Ramos on 29/01/21.
 //
 
-import Foundation
+import UIKit
+
+protocol Reusable {
+    static var identifier: String { get }
+}
+
+extension UICollectionViewCell: Reusable {
+    static var identifier: String { return String(describing: self) }
+}
+
+protocol Displayable {
+    associatedtype DisplayType
+    var display: DisplayType? { get }
+
+    func configure(with displayItem: DisplayType)
+}
